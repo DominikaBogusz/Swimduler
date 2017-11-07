d580c40cbf7d58bb693268be10302a1d21fbf97c
Dominika <dominika.bogu.bogusz@gmail.com>
2017-11-07 22:26:29 +0100

    Changed context class

---
+++ Swimduler/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Swimduler.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Client_Group> Client_Groups { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+    }
+}
