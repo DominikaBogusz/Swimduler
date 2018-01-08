698e8780705607632449750ca935a26237e2d4b3
Dominika <dominika.bogu.bogusz@gmail.com>
2018-01-08 11:40:29 +0100

    Mended globalizaion, added custom errors

--- Swimduler/App_Start/FilterConfig.cs
+++ Swimduler/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Swimduler
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
---
+++ Swimduler/Controllers/ErrorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Swimduler.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CustomError(int id)
+        {
+            Response.TrySkipIisCustomErrors = true;
+            Response.StatusCode = id;
+
+            switch (id)
+            {
+                case 400:
+                    ViewBag.Message = "Żądanie nie może być zrealizowne. Sprawdź poprawność wprowadzonego adresu.";
+                    break;
+                case 404:
+                    ViewBag.Message = "Podany adres nie istnieje. Sprawdź, czy odwołujesz się do prawidłowego zasobu serwera.";
+                    break;
+                case 500:
+                    ViewBag.Message = "Nastąpił wewnętrzny błąd serwera. Nasi eksperci już nad nim pracują.";
+                    break;
+            }
+
+            return View();
+        }
+    }
+}
